f2a16fbc18fbd01cf9b2246aebfb41678f830270
Vulcan <vulcan.lee@gmail.com>
2024-06-02 21:16:37 +0800

    使用 Blazor 設計 Todo 元件1

---
+++ csBlazorTodo/csBlazorTodo/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace csBlazorTodo.Models;
+
+public class Todo
+{
+    public string Content { get; set; }
+    public Guid Id { get; set; }
+    public bool IsCompleted { get; set; }
+    public bool IsEditing { get; set; }
+}
