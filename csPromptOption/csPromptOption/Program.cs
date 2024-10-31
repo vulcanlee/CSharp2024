3ab591aed340ef85924719fcde85d5c883dc6399
Vulcan <vulcan.lee@gmail.com>
2024-10-31 08:00:43 +0800

    |csPromptOption|Azure OpenAI AOAI 2.0 : 14 Prompt 的各種參數使用與變化差異||

---
+++ csPromptOption/csPromptOption/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+namespace csPromptOption;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // 讀取環境變數 AOAILabKey 的 API Key
+        string apiKey = System.Environment.GetEnvironmentVariable("AOAILabKey");
+        AzureOpenAIClient azureClient = new(
+            new Uri("https://gpt4tw.openai.azure.com/"),
+            new System.ClientModel.ApiKeyCredential(apiKey));
+        ChatClient chatClient = azureClient.GetChatClient("gpt-4");
+
+        string userPrompt = "Say 'this is a test.'";
+        Console.WriteLine($"{DateTime.Now}  [User]: {userPrompt}");
+        ChatCompletion completion = chatClient.CompleteChat("Say 'this is a test.'");
+        Console.WriteLine($"Role : {completion.Role}");
+
+        foreach (var message in completion.Content)
+        {
+            Console.WriteLine($"{DateTime.Now} {message.Text}");
+        }
+        Console.WriteLine($"InputTokenCount : {completion.Usage.InputTokenCount}");
+        Console.WriteLine($"OutputTokenCount : {completion.Usage.OutputTokenCount}");
+        Console.WriteLine($"ReasoningTokenCount : {completion.Usage.OutputTokenDetails?.ReasoningTokenCount}");
+        Console.WriteLine($"TotalTokenCount : {completion.Usage.TotalTokenCount}");
+    }
+}
